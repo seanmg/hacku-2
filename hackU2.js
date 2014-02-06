6403523c3640082c53d7db246f362e5ad10541e8
seanmg <s.y.galvin@spartans.nsu.edu>
2014-02-06 13:31:11 -0700

    Q&A
    Added random question generation with answer save

--- hackU2.js
+++ hackU2.js
@@ -11,7 +11,8 @@ function randFromArray(){
 }
 
 function getCycle(){
-	var _accessToken, modelID, _firstCycle, _secondCycle, _thirdCycle;
+	var _accessToken, modelID, _firstCycle, _secondCycle, _thirdCycle, _questionCycle, _pick;
+	var _cycles = new Array();
     var _maxCycles = 100;
 
 	$.ajax({
@@ -21,7 +22,7 @@ function getCycle(){
         data: {
           grant_type: 'client_credentials',
           client_id: 'norfolk-state',
-          client_secret: '',
+          client_secret: '^EC3<eGt{8^G*l~YJJ2!.e#OO6)T4R',
         },
 		error: function(){
 			console.log('error on token');
@@ -30,7 +31,7 @@ function getCycle(){
             _accessToken = data.access_token;
 	        $.ajax({
 		        type: "GET",
-		        url: "https://apis.traderonline.com/v1.0.0-beta/cycles?makeId=2316294&hasPhoto=true&limit=" + _maxCycles + "&view=full",
+		        url: "https://apis.traderonline.com/v1.0.0-beta/cycles?makeId=2316294&hasPhoto=true&minPrice=500.00&limit=" + _maxCycles + "&view=full",
 		        cache: false,
                 headers: {'Authorization' : 'Bearer ' + _accessToken},
 		        dataType: 'json',
@@ -38,9 +39,16 @@ function getCycle(){
 			        console.log('error');
 		        },
 		        success: function(data){
-                   _firstCycle = data.result[Math.floor(Math.random() * (_maxCycles))]
-                   _secondCycle = data.result[Math.floor(Math.random() * (_maxCycles))]
-                   _thirdCycle = data.result[Math.floor(Math.random() * (_maxCycles))]
+					console.log(data.result);
+                   _firstCycle = data.result[Math.floor(Math.random() * (_maxCycles))];
+                   _secondCycle = data.result[Math.floor(Math.random() * (_maxCycles))];
+                   _thirdCycle = data.result[Math.floor(Math.random() * (_maxCycles))];
+				   _cycles[0] = _firstCycle;
+				   _cycles[1] = _secondCycle;
+				   _cycles[2] = _thirdCycle;
+				   _pick = Math.floor(Math.random() * (3));
+				   questionCycle = _cycles[_pick];
+				   $('#question').html('<p>' + "Which Harley costs $" + questionCycle.price + "!?" + '<p>');
 				   $('#cycle0').html(
                         '<p>' + _firstCycle.year + ' ' + _firstCycle.makeDisplayName + ' ' + _firstCycle.modelDisplayName + '</p>'
                         + '<img src="' + _firstCycle.photos[0].url + '?width=300" />'
@@ -53,6 +61,9 @@ function getCycle(){
                         '<p>' + _thirdCycle.year + ' ' + _thirdCycle.makeDisplayName + ' ' + _thirdCycle.modelDisplayName + '</p>'
                         + '<img src="' + _thirdCycle.photos[0].url + '?width=300" />'
                    );
+				   $('#results').html('<p>' + "The answer was #" + (_pick+1) + " which is a " + questionCycle.year +' ' + questionCycle.makeDisplayName + ' ' 
+						+ questionCycle.modelDisplayName + "!!" + '<p>'
+				   );
 		        }
 	        });
         },
