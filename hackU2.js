de2e2592d002a219a5ac3c31c79ec696db637b63
seanmg <s.y.galvin@spartans.nsu.edu>
2014-02-06 12:06:21 -0700

    Updated - New API
    Tied into real API

--- hackU2.js
+++ hackU2.js
@@ -11,16 +11,16 @@ function randFromArray(){
 }
 
 function getCycle(){
-	var _accessToken, _cycles, modelID;
+	var _accessToken, _cycles, modelID, _firstCycle, _secondCycle, _thirdCycle;
 
 	$.ajax({
         type: 'POST',
         dataType: 'json',
-        url: 'https://api-dev.traderonline.com/vlatest/token',
+        url: 'https://apis.traderonline.com/v1.0.0-beta/token',
         data: {
           grant_type: 'client_credentials',
-          client_id: 'test',
-          client_secret: 'test',
+          client_id: 'norfolk-state',
+          client_secret: '',
         },
 		error: function(){
 			console.log('error on token');
@@ -29,7 +29,7 @@ function getCycle(){
             _accessToken = data.access_token;
 	        $.ajax({
 		        type: "GET",
-		        url: "https://api-dev.traderonline.com/vlatest/cycles?makeId=2316294&hasPhoto=true&limit=500&view=full",
+		        url: "https://apis.traderonline.com/v1.0.0-beta/cycles?makeId=2316294&hasPhoto=true&limit=500&view=full",
 		        cache: false,
                 headers: {'Authorization' : 'Bearer ' + _accessToken},
 		        beforSend: function (xhr) {
@@ -40,13 +40,10 @@ function getCycle(){
 		        },
 		        success: function(data){
                    _cycles = data.result;
-				   console.log(data.result);
-				   console.log(data);
-				   console.log(_cycles);
-				   var firstCycle = randFromArray();
-				   var secondCycle = randFromArray();
-				   var thirdCycle = randFromArray();
-				   $('#cycle0').html('<img src="' + _cycles[firstCycle].photos[0].url + '?width=300" />');
+				   firstCycle = randFromArray();
+				   secondCycle = randFromArray();
+				   thirdCycle = randFromArray();
+				   $('#cycle0').html('<img src="' + _cycles[firstCycle].photos[0].url + '?width=300" />'); 
 				   $('#cycle1').html('<img src="' + _cycles[secondCycle].photos[0].url + '?width=300" />');
 				   $('#cycle2').html('<img src="' + _cycles[thirdCycle].photos[0].url + '?width=300" />');
 		        }
